8d6156de1ade7d59a4e567e913dd5e0edecd4698
dawidMrozik <dawid.mrozik0@gmail.com>
2019-05-17 12:13:13 +0200

    HOC for pageContext added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -101,6 +101,13 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        [action.payload.itemType]: [
+          ...state[action.payload.itemType].filter(item => item.id !== action.payload.id),
+        ],
+      };
     default:
       return state;
   }
